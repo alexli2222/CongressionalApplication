48dab16d8b74cab572689842327286fa2aa8941b
Alex Li <lialex764@gmail.com>
2024-08-08 18:24:39 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.alexli6.congressionalapplication {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.alexli6.congressionalapplication to javafx.fxml;
+    exports org.alexli6.congressionalapplication;
+}
